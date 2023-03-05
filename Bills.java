62a3980f8c360a89709ff7a108b2f25d4c2fd7a3
Ben <119820886+Bens-Jammin@users.noreply.github.com>
2023-03-05 16:35:11 -0500

    Update Bills.java

--- Bills.java
+++ Bills.java
@@ -1,7 +1,7 @@
 import java.time.LocalDate;
 
 public class Bills{
-
+    private static final String SEPARATOR = "##########";
     String name;
     double amount;
     String category;
@@ -18,5 +18,20 @@ public class Bills{
 
     public String getBillName(){
         return this.name;
+    }
+
+    public void displayBill(){
+        System.out.println(SEPARATOR);
+        System.out.println(name+" payment to "+recipient);
+        System.out.println(" >> Total amount due: $"+amount);
+        System.out.println(" >> Due by "+dueDate.toString());
+        if(isOverdue){
+            System.out.println("*****");
+            System.out.println("WARNING: this payment is currently OVERDUE");
+            System.out.println("*****");
+        }else{
+            System.out.println("This payment is currently not overdue");
+        }
+        System.out.println(SEPARATOR);
     }
 }
