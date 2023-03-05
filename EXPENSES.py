b011b384772d9fa1b10bf177d51cf9d856f57f5d
Ben <119820886+Bens-Jammin@users.noreply.github.com>
2023-03-05 15:15:21 -0500

    Update EXPENSES.py

--- EXPENSES.py
+++ EXPENSES.py
@@ -7,4 +7,14 @@ class Expenses:
         self.category = category
         self.day = datetime.date.today()
 
+    def getName(self):
+        return self.name
     
+    def getAmount(self):
+        return self.amount
+    
+    def getCategory(self):
+        return self.category
+    
+    def getDate(self):
+        return self.date
