819e5e68581983c72692f81346d9625b2b43a3a6
Ben <119820886+Bens-Jammin@users.noreply.github.com>
2023-03-06 15:28:02 -0500

    Update User.java

--- User.java
+++ User.java
@@ -1,84 +1,77 @@
-import java.util.Scanner;
+import java.time.LocalDate;
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        User user = null;
-        boolean loggedIn = false;
-        
-        while (true) {
-            if (!loggedIn) {
-                System.out.println("Enter '1' to sign up, '2' to log in, or '3' to exit:");
-                int choice = scanner.nextInt();
-                
-                switch (choice) {
-                    case 1:
-                        System.out.println("Enter a username:");
-                        String username = scanner.next();
-                        System.out.println("Enter a password:");
-                        String password = scanner.next();
-                        user = new User(username, password);
-                        System.out.println("Sign up successful.");
-                        loggedIn = true;
-                        // add a thing to save class instance info
-                        break;
-                    case 2:
-                        System.out.println("Enter your username:");
-                        String loginUsername = scanner.next();
-                        System.out.println("Enter your password:");
-                        String loginPassword = scanner.next();
-                        /* TODO:
-                        1. make attemptSignin a static class, checks all saved class data if its available
-                        2. if available, user = new User(user, pwd);
-                        3. refactor? this looks like I can refactor it
-                        */
-                        if (user != null && user.attemptSignin(loginUsername, loginPassword)) {
-                            System.out.println("Login successful.");
-                            loggedIn = true;
-                        } else {
-                            System.out.println("Invalid username or password.");
-                        }
-                        break;
-                    case 3:
-                        System.out.println("Exiting...");
-                        System.exit(0);
-                    default:
-                        System.out.println("Invalid input. Try again.");
-                        break;
-                }
-            } else {
-                // user is logged in, display options here
-                System.out.println("Enter '1' to add an expense, '2' to remove an expense, '3' to add a bill, '4' to remove a bill, '5' to display all expenses, '6' to display all bills, or '7' to log out:");
-                int choice = scanner.nextInt();
-                
-                switch (choice) {
-                    case 1:
-                        // add expense logic here
-                        break;
-                    case 2:
-                        // remove expense logic here
-                        break;
-                    case 3:
-                        // add bill logic here
-                        break;
-                    case 4:
-                        // remove bill logic here
-                        break;
-                    case 5:
-                        // display all expenses logic here
-                        break;
-                    case 6:
-                        // display all bills logic here
-                        break;
-                    case 7:
-                        System.out.println("Logging out...");
-                        loggedIn = false;
-                        break;
-                    default:
-                        System.out.println("Invalid input. Try again.");
-                        break;
-                }
+public class User{
+    public static final int INCREMENT = 10;
+    Expenses[] expenses;
+    int expensesCount;
+    Bills[] bills;
+    int billsCount;
+
+    String username;
+    String password;
+
+
+    public User(String username, String password){
+        this.expenses = new Expenses[10];
+        this.bills = new Bills[10];
+        this.username = username;
+        this.password = password;
+    }
+
+    public void addExpense(String name, double amount, String category){
+        this.expenses[expensesCount++] = new Expenses(name, amount, category);
+    }
+    
+
+    public void removeExpense(String name){
+        int emptySpot = 0;
+        for(int i=0; i<expenses.length;i++){
+            if (expenses[i].getExpenseName() == name){
+                expenses[i] = null;
+                emptySpot = i;
             }
         }
+        for(int i = emptySpot+1; i<expenses.length;i++){
+            expenses[i-1] = expenses[i];
+        }
+    }
+
+
+    public void addBill(String name, double amount, String category, String recipient, LocalDate dueDate){
+        this.bills[billsCount++] = new Bills(name, amount, category, recipient, dueDate);
+    }
+
+
+    public void removeBill(String name){
+        int emptySpot = 0;
+        for(int i=0; i<bills.length;i++){
+            if (bills[i].getBillName() == name){
+                bills[i] = null;
+                emptySpot = i;
+            }
+        }
+        for(int i = emptySpot+1; i<bills.length;i++){
+            bills[i-1] = bills[i];
+        }
+    }
+
+
+    public void displayAllExpenses(){
+        for(int i=0; i<expensesCount; i++){
+            expenses[i].displayBill();
+            System.out.print("\n");
+        }
+    }
+
+
+    public void displayAllBills(){
+        for(int i=0; i<billsCount; i++){
+            bills[i].displayBill();
+            System.out.print("\n");
+        }
+    }
+
+    public boolean attemptSignin(String username, String password){
+        return this.username == username && this.password == password;
     }
 }
