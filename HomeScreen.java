6a78a5e521729b3188ac2c66fe420dce7c616073
Ben <benemiller994@gmail.com>
2023-03-06 12:44:46 -0500

    none

---
+++ HomeScreen.java
@@ -0,0 +1,7 @@
+public class HomeScreen{
+
+    private static final String SEPERATOR = "=========";
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
--- mainTest.java
+++
@@ -1,8 +0,0 @@
-public class mainTest{
-
-    public static void main(String[] args) {
-        User ben = new User("ADMIN", "123");
-        System.out.println(ben.AttemptSignin("ADMIN", "123"));
-        System.out.println(ben.AttemptSignin("ADMIN", "122"));
-    }
-}
