c7e3d318edf61ec4e9777352b75dc78baa310bdb
Ben <benemiller994@gmail.com>
2023-03-22 13:29:43 -0400

    silly little desktop app in progress

--- DesktopApplication.java
+++ DesktopApplication.java
@@ -2,24 +2,21 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class DesktopApplication extends JFrame {
-
-    private JPanel contentPane;
-
-    public DesktopApplication() {
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setBounds(100, 100, 300, 200);
-        setTitle("Hello, World!");
-
-        contentPane = new JPanel();
-        setContentPane(contentPane);
-
-        JLabel label = new JLabel("Testing desktop application");
-        contentPane.add(label);
-    }
-
-    public static void main(String[] args) {
-        DesktopApplication frame = new DesktopApplication();
+public class DesktopApplication extends JFrame 
+{
+        public static void main(String[] args)
+    {
+        // initialize the frame
+        JFrame frame = new JFrame("Frame Title");
         frame.setVisible(true);
+        int width = 800;
+        int height = 400;
+        frame.setSize(width, height);
+
+        JLabel usernameLabel = new JLabel("Username: ");
+        frame.add(usernameLabel, );
+        JLabel passwordLabel = new JLabel("Password: ");
+        frame.add(passwordLabel);
+
     }
 }
